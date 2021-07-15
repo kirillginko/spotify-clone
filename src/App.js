bb713a5898c629cd72f547716fcce661fbadead6
Kirill Ginko <kirillginko@gmail.com>
2021-07-15 18:40:50 -0400

    basic outline and React Context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromResponse } from "./spotify";
+import Player from "./Player";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [{ user, token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,68 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  token:
+    "BQCw9PWzGEURNvVT12J649o2EIuHruAKfClMRARrDN2knzgQPa3ihgNDSq7asL-F_G3uDVkGY3joZplcj44kpKxwb8jSohRozLrGuHn6S0IPWpp6Wc0LUH_Eqh9rg6ynx5KpL2UaUhzILMaYqN5onxzepuyi28ifiQ",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
